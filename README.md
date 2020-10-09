045fcfcc05f7f831bc5ce47dc5d2c6fa40789e96
QcFe <io@qcfe.me>
2020-10-09 16:14:49 +0200

    Workspace setup test

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # remotebackup
+
+### Dev setup
+
+(assuming ubuntu)
+```sudo apt install libboost-all-dev```
---
+++ rb_client/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <boost/asio.hpp>
+
+using namespace boost::asio;
+using ip::tcp;
+using std::string;
+using std::cout;
+using std::endl;
+
+int main(int, char**) {
+    std::cout << "Hello, world!\n";
+}
+
